aa1ee0ef6acfcf6b95be1548243c0e3a648ac132
Némo Cazin <nemo.cazin@gmail.com>
2026-04-17 12:15:19 +0200

    fix: remove useless files

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,11 @@ import * as vscode from 'vscode';
 import { createDecoration, disposeDecoration } from './core/decoration';
 import { initializeDecorations } from './core/decorationUpdater';
 import { registerCommands } from './commands/commandRegistry';
-import { registerEventListeners } from './events/eventListeners';
 
 export function activate(context: vscode.ExtensionContext) {
     createDecoration();
     initializeDecorations();
     registerCommands(context);
-    registerEventListeners(context);
 }
 
 export function deactivate() {
