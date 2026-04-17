14d68975a6cfd0be97e35706ad85035e2ed753d0
Némo Cazin <nemo.cazin@gmail.com>
2026-04-17 12:26:43 +0200

    fix: seems that these eventListeners were important...

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,13 @@ import * as vscode from 'vscode';
 import { createDecoration, disposeDecoration } from './core/decoration';
 import { initializeDecorations } from './core/decorationUpdater';
 import { registerCommands } from './commands/commandRegistry';
+import { registerEventListeners } from './events/eventListeners';
 
 export function activate(context: vscode.ExtensionContext) {
     createDecoration();
     initializeDecorations();
     registerCommands(context);
+    registerEventListeners(context);
 }
 
 export function deactivate() {
